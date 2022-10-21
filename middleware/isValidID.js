56616587b77432c81be67256ba832abd27a5be2a
ViacheslavDedov <dedoff.v@gmail.com>
2022-10-21 12:03:38 +0300

    task 3

---
+++ middleware/isValidID.js
@@ -0,0 +1,20 @@
+const { isValidObjectId } = require("mongoose");
+
+const isValidId = (req, res, next) => {
+  const { contactId } = req.params;
+  const result = isValidObjectId(contactId);
+
+  if (!result) {
+    res.status(404);
+    const error = new Error(
+      res.json({
+        status: "error",
+        message: `id ${contactId} is not valid`,
+      })
+    );
+    return error;
+  }
+  next();
+};
+
+module.exports = isValidId;
